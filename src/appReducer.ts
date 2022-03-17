b2e519f4f778be4ee94c7a0814033f403e63dc88
simeonbain <bainsimeon@gmail.com>
2022-03-17 15:10:10 +1100

    Setup basic redux structure

---
+++ src/appReducer.ts
@@ -0,0 +1,5 @@
+export type AppState = {
+
+}
+
+export default function appState(): AppState {}
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createStore } from 'redux'; 
+import appState from './appReducer.js'; 
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+const app = createStore(appState); 
