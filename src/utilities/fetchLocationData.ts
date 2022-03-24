73860de36c82e19c590de2f07028b9aa9c1b256a
simeonbain <bainsimeon@gmail.com>
2022-03-24 15:16:38 +1100

    Update fetch from OpenWeatherMap geocode API to use the https version

--- src/utilities/fetchLocationData.ts
+++ src/utilities/fetchLocationData.ts
@@ -2,7 +2,7 @@ import appConstants from '../constants/appConstants';
 
 async function fetchLocationData(location: string): Promise<{ lat: number; lon: number }> {
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${appConstants.OPEN_WEATHER_API_KEY}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${appConstants.OPEN_WEATHER_API_KEY}`
   );
   if (!response.ok) {
     const message = `An error occured: ${response.status}`;
